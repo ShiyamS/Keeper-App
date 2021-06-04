e0ae1b14c32c3938832bcec33f85b7201901b65d
Shiyam <shiyams315@gmail.com>
2021-06-04 11:58:18 +0530

    Keeper app complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HighlightOutlinedIcon from '@material-ui/icons/HighlightOutlined';
+function Header() {
+  return (
+    <header>
+      <h1><HighlightOutlinedIcon/>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
